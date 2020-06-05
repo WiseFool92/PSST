4e6109a0648428eaf31ed46cd264cab6cfbbe51e
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-06-05 11:57:49 -0700

    populate Account & Flavors Controller

---
+++ Controllers/FlavorsController.cs
@@ -0,0 +1,103 @@
+using Microsoft.AspNetCore.Mvc;
+using PSST.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace PSST.Controllers
+{
+  [Authorize]
+  public class FlavorsController : Controller
+  {
+    private readonly PSSTContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
+
+    public FlavorsController(UserManager<ApplicationUser> userManager, PSSTContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    public async Task<ActionResult> Index()
+    {
+      var userId = this._userManager.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      return View(_db.Flavors.Where(x => x.User.Id == currentUser.Id).ToList());
+    }
+
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Flavor flavor)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      flavor.User = currentUser;
+      _db.Flavors.Add(flavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public async Task<ActionResult> Details(int id)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var thisFlavor = _db.Flavors
+        .Include(flavor => flavor.Treats)
+        .ThenInclude(join => join.Treat)
+        .Where(flavor => flavor.User.Id == currentUser.Id)
+        .FirstOrDefault(flavor => flavor.FlavorId == id);
+      if (thisFlavor != null)
+      {
+        return View(thisFlavor);
+      }
+    }
+
+    public async Task<ActionResult> Edit(int id)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var thisFlavor = _db.Flavors
+        .Where(r => r.User.Id == currentUser.Id)
+        .FirstOrDefault(flavors => flavors.FlavorId == id);
+      if (thisFlavor != null)
+      {
+          return View(thisFlavor);
+      }
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Flavor flavor)
+    {
+      _db.Entry(flavor).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");               
+    }
+
+    public async Task<ActionResult> Delete(int id)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var thisFlavor = _db.Flavors
+        .Where(r => r.User.Id == currentUser.Id)
+        .FirstOrDefault(flavors => flavors.FlavorId == id);
+      if (thisFlavor != null)
+      {
+        return View(thisFlavor);
+      }
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisFlavor = _db.Flavors
+        .FirstOrDefault(flavors => flavors.FlavorId == id);
+      _db.Flavors.Remove(thisFlavor);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
