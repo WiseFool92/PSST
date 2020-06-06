02a1c8b2ceef1dd618d5e7c1588ed30947772f24
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-06-05 17:00:15 -0700

    test public display view

--- Controllers/DisplayController.cs
+++ Controllers/DisplayController.cs
@@ -12,10 +12,25 @@ namespace PSST.Controllers
 {
   public class DisplayController : Controller
   {
-    [HttpGet("/")]
+    public readonly PSSTContext _db;
+
+    public DisplayController(PSSTContext db)
+    {
+      _db = db;
+    }
     public ActionResult Index()
     {
       return View();
+    }
+    public ActionResult Index()
+    {
+      List<Flavor> model = _db.Flavors.ToList();
+      return View(model);
+    }
+    public ActionResult Index()
+    {
+      List<Treat> model = _db.Treats.ToList();
+      return View(model);
     }
   }
 }
