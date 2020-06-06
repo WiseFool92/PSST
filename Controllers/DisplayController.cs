620f156ec98a8579dc5fd4b91071879764976fde
Nathan Watkins Hoagland <watkins92@gmail.com>
2020-06-05 17:49:53 -0700

    I was trying to create a ViewModel that held the data for my two classes treat & flavor to then pull the data into my Display View inorder for non authorized viewers to observe the list of items. Since my other pages req authorization to view/create/edit/ & delete. Originally I wanted this to be viewed and populated on my Home Index page but I ran into the errors of only being allowed one @model statement per page. Halp! Ifeel so close and yet so far away

--- Controllers/DisplayController.cs
+++ Controllers/DisplayController.cs
@@ -1,36 +1,42 @@
-using Microsoft.AspNetCore.Mvc;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authorization;
-using System.Threading.Tasks;
-using System.Security.Claims;
-using System.Linq;
-using PSST.Models;
+// using Microsoft.AspNetCore.Mvc;
+// using System.Collections.Generic;
+// using Microsoft.EntityFrameworkCore;
+// using Microsoft.AspNetCore.Identity;
+// using Microsoft.AspNetCore.Authorization;
+// using System.Threading.Tasks;
+// using System.Security.Claims;
+// using System.Linq;
+// using PSST.Models;
 
-namespace PSST.Controllers
-{
-  public class DisplayController : Controller
-  {
-    public readonly PSSTContext _db;
+// namespace PSST.Controllers
+// {
+//   public class DisplayController : Controller
+//   {
+//     public readonly PSSTContext _db;
 
-    public DisplayController(PSSTContext db)
-    {
-      _db = db;
-    }
-    public ActionResult Index()
-    {
-      return View();
-    }
-    public ActionResult Index()
-    {
-      List<Flavor> model = _db.Flavors.ToList();
-      return View(model);
-    }
-    public ActionResult Index()
-    {
-      List<Treat> model = _db.Treats.ToList();
-      return View(model);
-    }
-  }
-}
+//     public DisplayController(PSSTContext db)
+//     {
+//       _db = db;
+//     }
+//     public ActionResult Index()
+//     {
+//       return View();
+//     }
+
+    // Was testing different options since you can only have one index route
+
+    // public ActionResult Index()
+    // {
+    //   List<Flavor> model = _db.Flavors.ToList();
+    //   List<Treat> model = _db.Treats.ToList();
+    //   return View(model);
+    // }
+    // public ActionResult Index()
+    // {
+    //   List<Treat> model = _db.Treats.ToList();
+    //   return View(model);
+    // }
+
+    
+//   }
+// }
--- ViewModels/DisplayViewModel.cs
+++ ViewModels/DisplayViewModel.cs
@@ -1,12 +1,12 @@
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+// using System.Collections.Generic;
+// using System.ComponentModel.DataAnnotations;
 
-namespace PSST.Models
-{
-  public class Display
-  {
-    public Flavor Flavor { get; set; }
-    public Treat Treat { get; set; }
-    public FlavorTreat FlavorTreat { get; set; }
-  }
-}
+// namespace PSST.Models
+// {
+//   public class Display
+//   {
+//     public Flavor Flavor { get; set; }
+//     public Treat Treat { get; set; }
+//     public FlavorTreat FlavorTreat { get; set; }
+//   }
+// }
